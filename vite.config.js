743d72b6404d0c45d3bc3f1b23a5fc826d0626fd
Nash Cabia <cabians@student.national-u.edu.ph>
2026-05-31 13:20:14 +0800

    Initial clean project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/JustiFi/',
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
+export default defineConfig(() => {
+  const basePath = process.env.BASE_PATH || '/';
+
+  return {
+    base: basePath,
+    plugins: [react()],
+    server: {
+      port: 3000
+    }
+  };
 });
